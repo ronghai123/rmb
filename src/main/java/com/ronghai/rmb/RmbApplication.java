da930bb37a5877c20adbc1ef1d8c6d1e41d4631a
樊荣海 <ronghai.fan@ele.me>
2018-05-17 22:25:18 +0800

    SpringMVC init

---
+++ src/main/java/com/ronghai/rmb/RmbApplication.java
@@ -0,0 +1,11 @@
+package com.ronghai.rmb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmbApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RmbApplication.class, args);
+    }
+}
